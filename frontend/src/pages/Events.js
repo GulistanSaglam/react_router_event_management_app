568e7d82b4925fb1eb899904c65cec27d23e2e81
Gulistan Saglam <gulistansglm@gmail.com>
2023-11-20 01:22:39 +0300

    first commit

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,22 @@
+import { useLoaderData, json } from 'react-router-dom';
+import EventsList from '../components/EventsList';
+
+function EventsPage() {
+    const data = useLoaderData();
+    const events = data.events;
+
+    return <EventsList events={events} />;
+}
+
+export default EventsPage;
+
+export async function loader() {
+    const response = await fetch('http://localhost:8080/events');
+    if (!response.ok) {
+        return json({ message: 'Could not fetch events.' }, {
+            status: 500,
+        });
+    } else {
+        return response;
+    }
+}
